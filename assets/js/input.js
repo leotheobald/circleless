fcef706fba0b2d431cf7b09443ba16336765c76a
Leo Theobald <leotheo@gmail.com>
2016-09-24 15:02:09 +0100

    added html snippet for basket count plus some less styling

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-    console.log( "ready!" );
-
-    // Take the form input value and apply it to cthe p** class and span text.
-    //bbb;
-
-});
+// $(document).ready(function() {
+//     console.log( "ready!" );
+//
+//     // Take the form input value and apply it to cthe p** class and span text.
+//     //bbb;
+//
+// });
